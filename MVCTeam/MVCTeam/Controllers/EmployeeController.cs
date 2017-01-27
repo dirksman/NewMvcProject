10fdb4ef6f141092e1176b822e1ade8df64251d1
dirksman <kobber@users.noreply.github.com>
2017-01-27 12:39:26 -0800

    Employee Edit
    edit

--- MVCTeam/MVCTeam/Controllers/EmployeeController.cs
+++ MVCTeam/MVCTeam/Controllers/EmployeeController.cs
@@ -19,5 +19,15 @@ namespace MVCTeam.Controllers
 
             return View(Employees.uspUpdateLeaveBalances().ToList());
         }
+
+        public ActionResult EmployeeEdit(int id)
+        {
+            //var Emp = Employees.People.Select(a => new { a.FirstName, a.LastName, a.BusinessEntityID }).ToList();
+            //var Emp = Employees.uspUpdateLeaveBalances()
+            //    .Select(e => new { e.FirstName, e.LastName, e.ID }).ToList();
+
+            var ds = Employees.uspUpdateLeaveBalances().SingleOrDefault(c => c.ID == id);
+            return View(ds);
+        }
     }
 }
